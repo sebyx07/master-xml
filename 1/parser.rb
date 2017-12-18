9d6aa787363d700274853778aa54bcc7a34043a5
sebi <gore.sebyx@yahoo.com>
2017-12-18 18:57:23 +0200

    update

--- 1/parser.rb
+++ 1/parser.rb
@@ -1,10 +1,12 @@
 require 'nokogiri'
 require 'pry'
 require 'ostruct'
+require 'saxerator'
 
 class User < OpenStruct
 end
 
+parser = Saxerator.parser(File.new('persons.xml'))
 
 doc = File.open('persons.xml') do |f|
   Nokogiri::Slop(f)
@@ -20,5 +22,16 @@ persons = doc.css('person').map do |node|
   User.new(attrs)
 end
 
+p 'DOM PARSER'
 p persons
+
+persons_sax = []
+
+parser.for_tag(:person).each do |person|
+  persons_sax << User.new(name: person['name'], age: person['age'])
+end
+
+p 'SAX PARSER'
+p persons_sax
+
 
